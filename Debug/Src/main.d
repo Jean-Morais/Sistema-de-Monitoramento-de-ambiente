db31a87c2802146bb7f49a1f25e8a35c0ffbe60c
Jean Morais da Silva <jean07morais@gmail.com>
2025-08-01 17:21:21 -0300

    Adicionando código da main

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -6,9 +6,14 @@ Src/main.o: ../Src/main.c \
  /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Include/cmsis_compiler.h \
  /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Include/cmsis_gcc.h \
  /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/INIT.h \
+ /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/I2C.h \
+ /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/SPI.h \
  /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/BMP280.h \
  /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/NRF24L01.h \
- /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/LDR.h
+ /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/LDR.h \
+ /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/TIMER.h \
+ /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/USART.h
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Include/core_cm3.h:
@@ -16,6 +21,11 @@ Src/main.o: ../Src/main.c \
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Include/cmsis_compiler.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Include/cmsis_gcc.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+/home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/INIT.h:
+/home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/I2C.h:
+/home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/SPI.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/BMP280.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/NRF24L01.h:
 /home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/LDR.h:
+/home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/TIMER.h:
+/home/jeanm/Documentos/workspace-stm/Projeto\ Final\ -\ monitoramento\ de\ ambiente/Inc/USART.h:
--- Src/main.c
+++ Src/main.c
@@ -1,24 +1,58 @@
 /**
  ******************************************************************************
- * @file           : main.c
- * @author		   : Jean Morais
- * 					 Joao Pedro
- * 					 Gabriel
+ * main
  */
 
 
+#include <stdio.h>
 #include <stdint.h>
 #include "stm32f1xx.h"
+#include "INIT.h"
+#include "I2C.h"
+#include "SPI.h"
 #include "BMP280.h"
 #include "NRF24L01.h"
 #include "LDR.h"
+#include "TIMER.h"
+#include "USART.h"
 
 
-int main(void){
-
-	for(;;);
+int main(void) {
+	enable_clock();
+	enable_timer();
+	enable_gpio();
+	enable_spi(SPI1);
+	I2C1_Init();
+	enable_LDR();
+	enable_usart();
+	nrf_config();  // configuração básica
+	BMP280_Init();
+	uint32_t photo;
+	uint8_t temp[3];
+	uint8_t payload[6];
+	uint32_t temperatura;
+	char buffer1[20];
+	char buffer2[20];
+//
+	while(1){
+		delay_us(5000); // 1 segundo
+		temperatura = BMP280_ReadTemperature(temp);
+		LDR_read(&photo);
+		sprintf(buffer1, "%lu\r\n", photo); // converte uint32_t para string com quebra de linha
+		sprintf(buffer2, "%lu\r\n", temperatura);
+		uart1_write_string("photo Sensor \r\n");
+		uart1_write_string(buffer1);
+		uart1_write_string("Temperatura \r\n");
+		uart1_write_string(buffer2);
+		payload[0] = temp[0];
+		payload[1] = temp[1];
+		payload[2] = temp[2];
+		payload[3] = (uint8_t)(photo & 0xFF);
+		payload[4] = (uint8_t)((photo >> 8) & 0xFF);
+		payload[5] = (uint8_t)((photo >> 16) & 0xFF);
+		send_payload(payload, 6); // envia 2 bytes
+	}
 }
-
 
 
 
